843866980546191b389ce54988550cdde0dbd80f
Tyler Bach <Tbach2@my.wctc.edu>
2022-03-03 22:16:47 -0800

    Initial commit

---
+++ src/main/java/edu/wctc/distjavazodiac/service/BasicMonthListService.java
@@ -0,0 +1,38 @@
+package edu.wctc.distjavazodiac.service;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import edu.wctc.distjavazodiac.entity.Fortune;
+import edu.wctc.distjavazodiac.entity.Month;
+import edu.wctc.distjavazodiac.repo.MonthRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.PostConstruct;
+import java.io.IOException;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+@Service
+public class BasicMonthListService implements MonthListService {
+    private List<Month> monthList;
+    private MonthRepository monthRepository;
+
+    @Autowired
+    public BasicMonthListService(MonthRepository monthRepository){
+        this.monthRepository = monthRepository;
+    }
+
+    @Override
+    public List<Month> getMonths() {
+        return monthList;
+    }
+
+    @PostConstruct
+    public void initMonths() {
+        List<Month> list = new ArrayList<>();
+        monthRepository.findAll().forEach(list::add);
+        monthList = list;
+    }
+}
